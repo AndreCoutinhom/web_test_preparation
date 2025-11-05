f93e9cdeb8aa110c2b72fb536841cc6442283c11
André Coutinho de Macedo Silva <91290799+AndreCoutinhom@users.noreply.github.com>
2025-11-05 16:07:42 -0300

    Update array_control.js

--- array_control.js
+++ array_control.js
@@ -1,9 +1,24 @@
-let humans = ["Jax", "Ragatha", "Gangle", "Kingur", "Zooble", "Kaufmo"];
+let humans = ["Jax", "Ragatha", "Gangle", "Caim", "Kingur", "Zooble", "Bubble", "Kaufmo"];
 
 console.log(humans);
+
+console.log("Wait. Caim and Bubble and humans and Kaufmo... isn't human anymore. Also, Pomni is missing!");
+
+humans.splice(3, 1);
+humans.splice(6, 1);
 
 humans.unshift("Pomni");
 
 humans.pop();
 
 console.log(humans);
+
+for (let i of humans) {
+  document.write("Tem " + i + "<br>");
+}
+
+/* OU
+for (let i in humans) {
+  document.write("Tem " + humans[i] + "<br>")
+}
+*/
